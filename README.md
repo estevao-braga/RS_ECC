56a7a95e543a35685ee8015b2745445caa5a749e
estevao1805 <estevaobraga50@gmail.com>
2023-12-13 20:26:39 -0300

    projeto finalizado

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# Criptografia de Curva Elíptica
+# RS_ECC
+
+
+Este projeto visa implementar o ECDSA (Elliptic Curve Digital Signature Algorithm) em Rust. O objetivo é criar uma implementação direta e funcional do ECDSA, abordando a geração de chaves, assinatura e verificação por meio de operações em curvas elípticas além de adquirir conhecimento prático e teórico em criptografia. 
+
+## Equação Geral
+A equação de Weierstrass refere-se a uma forma específica de equação que descreve uma curva elíptica cujo a forma geral definida sobre um corpo $K$ (como os números reais ou complexos) é dada por: $$y^2= x^3 + ax + b$$ onde $4a^3 + 27b^3$ não pode ser igual a $0$.
 <br>
+A equação de Weierstrass descreve uma curva elíptica no plano coordenado, e a sua forma específica é escolhida de modo a garantir que a curva tenha propriedades importantes, como a associatividade da operação de soma de pontos e a existência de um ponto de identidade. As constantes 
+$a$ e $b$ afetam a localização e a inclinação da curva elíptica no plano.
+
+
+## Operação de Soma
+
+Podemos definir a operação de soma de dois pontos $P(x_1, y_1)$ e $Q(x_2, y_2)$ pertencentes a uma curva elíptica $E(K)$ como 
+$$P(x_1, y_1) + Q(x_2, y_2) = R(x_3, y_3)$$
+
+$$λ = (y₂ - y₁)(x₂ - x₁)⁻¹, P ≠ Q$$
+$$λ = (3x₁² + A)(2y₁)⁻¹, P = Q$$
+$$x₃ = λ² - x₁ - x₂$$
+$$y₃ = λ(x₁ - x₃) - y₁$$
+
+
+### Multiplicação por escalar e o problema do logaritmo discreto
+A multiplicação por escalar em curvas elípticas, especialmente quando implementada com o algoritmo Double and Add, desempenha um papel crucial em algoritmos criptográficos como o ECDSA. Este processo envolve a multiplicação de um ponto na curva por um escalar (um número inteiro), resultando em um novo ponto na curva.
+<br>
+A segurança de algoritmos baseados em curvas elípticas, como o ECDSA repousa na dificuldade do Problema do Logaritmo Discreto em curvas elípticas. Esse problema, em termos simples, consiste em encontrar o expoente $d$ na equação $Q=d*G$, onde $G$ é um ponto base conhecido e $Q$ é um ponto qualquer na curva. O algoritmo de multiplicação por escalar é fundamental nesse contexto, já que a dificuldade de inverter essa operação é o que confere a segurança do sistema. 
+
+### Ponto de Identidade:
+
+A curva elíptica tem um ponto especial chamado ponto de identidade, denotado como 0. É um ponto fictício que serve como elemento neutro para a adição.
+
+### Estrutura de Grupo: 
+
+A soma de pontos em uma curva elíptica possui propriedades de grupo, incluindo associatividade, comutatividade e existência do elemento inverso. Essas propriedades são fundamentais para a segurança e eficiência dos algoritmos de criptografia baseados em curvas elípticas.
+
+## ECDSA (Elliptic Curve Digital Signature Algorithm)
+
+O ECDSA, ou Elliptic Curve Digital Signature Algorithm, é um algoritmo de assinatura digital baseado em curvas elípticas. Este algoritmo faz parte da família de algoritmos de criptografia de chave pública e é amplamente utilizado para garantir a autenticidade e a integridade de dados em sistemas de comunicação seguros.
+
+### 1 - Geração de Chaves
+Para fins didádicos chamaremos os dois agentes nesse exemplo de Alice e Bob. 
+<br>Primeiramente é definida a curva $E(Z_p)$ com a ordem de $Z_p$ igual a um $q$ primo sobre a qual vamos trabalhar. E então escolhemos um elemento gerador $A \in E(Z_p)$. 
+<br>
+- Alice gera uma chave privada aleatória $d \in Z_p$ de forma que $0 < d < q$. 
+- Depois Alice calcula a chave pública $B = d*A$ a partir de chave privada e do gerador do grupo.
+
+### 2 - Assinatura da mensagem
+- Primeiro selecionamos um $k_e$ aleatório, $0 < k_e < q$.
+- $R(x_r, y_r) = K_e*A$, $r = x_r$.
+- $S = (hash(message) + d*r)*K_e⁻¹\mod q$.
+- $Signature = (r, s)$.
+
+### 3 - Verificação da assinatura
+- $u_1 = S⁻¹ * hash(message) \mod q$.
+- $u_2 = S⁻¹* r \mod q$.
+- $P(x_p, y_p) = u_1*A + u_2*B$.
+- Se $x_p = r$ a assinatura está verificada.
+
+### 4 - Prova
+$$S = (hash(message) + d*r)*K_e⁻¹\mod q$$
+$$k_e = S⁻¹*hash(message) + S⁻¹*d*r \mod q$$
+$$K_e*A = u_1*A + u_2*d*A \mod q$$
+$$K_e*A = u_1*A + u_2*B \mod q$$
+$$K_e*A = R(x_r, y_r) = u_1*A + u_2*B \mod q = P(x_p, y_p) $$
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_imports)]
+
 use ec_generic::{EllipticCurve, FiniteField, Point};
 use num_bigint::{BigUint, RandBigInt};
 use rand::{self, Rng};
@@ -63,7 +65,141 @@ impl ECDSA {
         panic!("The random point R should not be the identity");
     }
 
-    pub fn verification(&self, hash: &BigUint, pub_key: &Point, signature: &BigUint) -> bool {
-        todo!()
+    pub fn verification(
+        &self,
+        hash: &BigUint,
+        pub_key: &Point,
+        signature: &(BigUint, BigUint),
+    ) -> bool {
+        assert!(
+            hash < &self.q_order,
+            "Hash is bigger than the order of the EC group"
+        );
+
+        let (r, s) = signature;
+        let s_inv = FiniteField::inv_mult_prime(&s, &self.q_order).unwrap();
+        let u1 = FiniteField::mult(&s_inv, hash, &self.q_order).unwrap();
+        let u2 = FiniteField::mult(&s_inv, r, &self.q_order).unwrap();
+        let u1a = self.elliptic_curve.scalar_mul(&self.a_gen, &u1).unwrap();
+        let u2b = self.elliptic_curve.scalar_mul(&pub_key, &u2).unwrap();
+        let p = self.elliptic_curve.add(&u1a, &u2b).unwrap();
+
+        if let Point::Coor(xp, _) = p {
+            return &xp == r;
+        }
+        panic!("Point P = u1 + u2 cannot be the identity")
+    }
+
+    pub fn generate_hash_less_than(&self, message: &str, max: &BigUint) -> BigUint {
+        let digest = digest(message);
+        let hash_bytes = hex::decode(digest).expect("Could not convert hash to Vec<u8>");
+        let hash = BigUint::from_bytes_be(&hash_bytes)
+            .modpow(&BigUint::from(1u32), &(max - BigUint::from(1u32)));
+        let hash = hash + BigUint::from(1u32);
+        hash
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::hash;
+
+    use super::*;
+
+    #[test]
+    fn test_sign_verify() {
+        let elliptic_curve = EllipticCurve {
+            a: BigUint::from(2u32),
+            b: BigUint::from(2u32),
+            p: BigUint::from(17u32),
+        };
+        let a_gen = Point::Coor(BigUint::from(5u32), BigUint::from(1u32));
+        let q_order = BigUint::from(19u32);
+
+        let ecdsa = ECDSA {
+            elliptic_curve,
+            a_gen,
+            q_order,
+        };
+
+        let priv_key = BigUint::from(7u32);
+        let pub_key = ecdsa.generate_pub_key(&priv_key);
+
+        let k_random = BigUint::from(18u32);
+
+        let message = "Bob -> 1 BTC -> Alice";
+        let hash = ecdsa.generate_hash_less_than(&message, &ecdsa.q_order);
+
+        let signature = ecdsa.sign(&hash, &priv_key, &k_random);
+        let verify_result = ecdsa.verification(&hash, &pub_key, &signature);
+
+        assert!(verify_result, "Verification should sucess");
+    }
+
+    #[test]
+    fn test_sign_verify_tempered_message() {
+        let elliptic_curve = EllipticCurve {
+            a: BigUint::from(2u32),
+            b: BigUint::from(2u32),
+            p: BigUint::from(17u32),
+        };
+        let a_gen = Point::Coor(BigUint::from(5u32), BigUint::from(1u32));
+        let q_order = BigUint::from(19u32);
+
+        let ecdsa = ECDSA {
+            elliptic_curve,
+            a_gen,
+            q_order,
+        };
+
+        let priv_key = BigUint::from(7u32);
+        let pub_key = ecdsa.generate_pub_key(&priv_key);
+
+        let k_random = BigUint::from(18u32);
+
+        let message = "Bob -> 1 BTC -> Alice";
+        let hash = ecdsa.generate_hash_less_than(&message, &ecdsa.q_order);
+        let signature = ecdsa.sign(&hash, &priv_key, &k_random);
+
+        let message = "Bob -> 2 BTC -> Alice";
+        let hash = ecdsa.generate_hash_less_than(&message, &ecdsa.q_order);
+        let verify_result = ecdsa.verification(&hash, &pub_key, &signature);
+
+        assert!(!verify_result, "Verification should fail");
+    }
+
+    #[test]
+    fn test_sign_verify_tempered_signature() {
+        let elliptic_curve = EllipticCurve {
+            a: BigUint::from(2u32),
+            b: BigUint::from(2u32),
+            p: BigUint::from(17u32),
+        };
+        let a_gen = Point::Coor(BigUint::from(5u32), BigUint::from(1u32));
+        let q_order = BigUint::from(19u32);
+
+        let ecdsa = ECDSA {
+            elliptic_curve,
+            a_gen,
+            q_order,
+        };
+
+        let priv_key = BigUint::from(7u32);
+        let pub_key = ecdsa.generate_pub_key(&priv_key);
+
+        let k_random = BigUint::from(4u32);
+
+        let message = "Bob -> 1 BTC -> Alice";
+        let hash = ecdsa.generate_hash_less_than(&message, &ecdsa.q_order);
+        let signature = ecdsa.sign(&hash, &priv_key, &k_random);
+        let (r, s) = signature;
+        let tempered_siganture = (
+            (r + BigUint::from(1u32)).modpow(&BigUint::from(1u32), &ecdsa.q_order),
+            s,
+        );
+
+        let verify_result = ecdsa.verification(&hash, &pub_key, &tempered_siganture);
+
+        assert!(!verify_result, "Verification should fail");
     }
 }
